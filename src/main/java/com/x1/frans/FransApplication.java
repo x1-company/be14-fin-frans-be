c05a60f0296addee9639defd84dce2390e9f0e62
cxzaqq <roy1228@naver.com>
2025-06-03 09:30:07 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/x1/frans/FransApplication.java
@@ -0,0 +1,13 @@
+package com.x1.frans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FransApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FransApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/x1/frans/FransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x1.frans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FransApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
