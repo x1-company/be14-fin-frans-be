aaa07e56935d6069d74b0d71fd4f8ddf257bfc02
JK-LEE98 <abc9541@gmail.com>
2025-06-11 14:17:51 +0900

    공급처, 자재 Controller의 Tag Annotation name 수정

--- src/main/java/com/x1/frans/product/query/controller/ProductQueryController.java
+++ src/main/java/com/x1/frans/product/query/controller/ProductQueryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "🐓 자재", description = "product")
+@Tag(name = "🍗 자재", description = "product")
 @Slf4j
 @RestController
 @RequestMapping("/api/hq/products")
--- src/main/java/com/x1/frans/supplier/command/application/controller/SupplierCommandController.java
+++ src/main/java/com/x1/frans/supplier/command/application/controller/SupplierCommandController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "공급처", description = "공급처 관련 API")
+@Tag(name = "🐔 공급처", description = "공급처 관련 API")
 @RestController
 @RequestMapping("/api/supplier")
 @Slf4j
