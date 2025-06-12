8917ebb1d497cf48cf12a1e7fbc0895e0bf44d8b
zi-won <aktos109@gmail.com>
2025-06-12 11:04:34 +0900

    🔨 refactor: controller 수정

--- src/main/java/com/x1/frans/warehouse/command/application/controller/WarehouseCommandController.java
+++ src/main/java/com/x1/frans/warehouse/command/application/controller/WarehouseCommandController.java
@@ -1,20 +1,24 @@
 package com.x1.frans.warehouse.command.application.controller;
 
-import com.x1.frans.warehouse.command.application.service.WarehouseCommandServiceImpl;
+import com.x1.frans.warehouse.command.application.service.WarehouseCommandService;
 import com.x1.frans.warehouse.command.application.service.dto.WarehouseCreateCommand;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/hq/warehouses")
-@RequiredArgsConstructor
 @Tag(name = "📦 창고", description = "warehouse")
 public class WarehouseCommandController {
 
-    private final WarehouseCommandServiceImpl warehouseCommandServiceImpl;
+    private final WarehouseCommandService warehouseCommandService;
+
+    @Autowired
+    public WarehouseCommandController(WarehouseCommandService warehouseCommandService) {
+        this.warehouseCommandService = warehouseCommandService;
+    }
 
     @PostMapping
     @Operation(
@@ -22,7 +26,7 @@ public class WarehouseCommandController {
             description = "창고 정보를 등록합니다. code(중복불가), name, address, 담당자 userId 필수."
     )
     public ResponseEntity<Long> create(@RequestBody WarehouseCreateCommand command) {
-        Long id = warehouseCommandServiceImpl.create(command);
+        Long id = warehouseCommandService.create(command);
         return ResponseEntity.ok(id);
     }
 }
