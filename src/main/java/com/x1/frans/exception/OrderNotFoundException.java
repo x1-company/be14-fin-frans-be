7a50d08b706a9535ca61d1f4c78aa8b0e214dfaf
mmmv41 <tlsalsrud5068@daum.net>
2025-06-12 09:29:33 +0900

    🚑 fix: 예외처리 수정

--- src/main/java/com/x1/frans/exception/OrderNotFoundException.java
+++ src/main/java/com/x1/frans/exception/OrderNotFoundException.java
@@ -3,12 +3,13 @@ package com.x1.frans.exception;
 import com.x1.frans.exception.enums.ErrorCode;
 
 public class OrderNotFoundException extends BaseCustomException {
-    @Override
-    public ErrorCode getErrorCode() {
-        return null;
-    }
 
     public OrderNotFoundException(String message) {
         super(message);
+    }
+
+    @Override
+    public ErrorCode getErrorCode() {
+        return ErrorCode.ORDER_NOT_FOUND;
     }
 }
