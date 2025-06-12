e3e4bba3ac85a7ea27e56e7aed526912ef425bd4
Sumin <emily9949@naver.com>
2025-06-12 18:40:04 +0900

    ✨ feat: 1주일 이내 배송완료된 주문 목록 조회

--- src/main/java/com/x1/frans/returns/query/service/ReturnQueryService.java
+++ src/main/java/com/x1/frans/returns/query/service/ReturnQueryService.java
@@ -1,4 +1,12 @@
 package com.x1.frans.returns.query.service;
 
+import com.x1.frans.returns.query.dto.ShippedOrderDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface ReturnQueryService {
+
+    List<ShippedOrderDTO> findDeliveredOrdersWithinLastWeek(Long userId, Long FranchiseId);
+
 }
--- src/main/java/com/x1/frans/returns/query/service/ReturnQueryServiceImpl.java
+++ src/main/java/com/x1/frans/returns/query/service/ReturnQueryServiceImpl.java
@@ -1,4 +1,24 @@
 package com.x1.frans.returns.query.service;
 
-public class ReturnQueryServiceImpl {
+import com.x1.frans.returns.query.dto.ShippedOrderDTO;
+import com.x1.frans.returns.query.repository.ReturnQueryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReturnQueryServiceImpl implements ReturnQueryService {
+
+    private final ReturnQueryMapper returnQueryMapper;
+
+    @Autowired
+    public ReturnQueryServiceImpl(ReturnQueryMapper returnQueryMapper) {
+        this.returnQueryMapper = returnQueryMapper;
+    }
+
+    @Override
+    public List<ShippedOrderDTO> findDeliveredOrdersWithinLastWeek(Long userId, Long franchiseId) {
+        return returnQueryMapper.findDeliveredOrdersWithinLastWeek(userId, franchiseId);
+    }
 }
