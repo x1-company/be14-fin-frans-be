e1d3edf4c9b0aaf27beb67f1df6295469b798192
mmmv41 <tlsalsrud5068@daum.net>
2025-06-13 15:36:33 +0900

    📚 Docs: swagger 관련 코드 추가

--- src/main/java/com/x1/frans/order/query/controller/HqOrderQueryController.java
+++ src/main/java/com/x1/frans/order/query/controller/HqOrderQueryController.java
@@ -6,6 +6,7 @@ import com.x1.frans.order.query.dto.OrderSearchPageResponseDto;
 import com.x1.frans.order.query.service.HqOrderQueryService;
 import com.x1.frans.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,10 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/hq/orders")
 @RequiredArgsConstructor
+@Tag(name = "📝 주문 조회", description = "주문 조회 관련 API")
 public class HqOrderQueryController {
+
     private final HqOrderQueryService orderQueryService;
 
     @GetMapping
+    @Operation(
+            summary = "주문 목록 조회",
+            description = "본인이 속한 부서가 관리하는 가맹점의 주문 목록을 조회합니다."
+    )
     public OrderSearchPageResponseDto searchOrdersPaged(
             @ModelAttribute OrderSearchConditionDto condition
     ) {
