bf5b33ce461337695513177e9b54fbf5c33819c1
oni128 <lyw283828@gmail.com>
2025-06-15 21:00:41 +0900

    ✨ feat: 결재 요청 DTO에 @NotNull 유효성 검증 추가

--- src/main/java/com/x1/frans/approval/command/application/dto/ApprovalDecisionRequestDTO.java
+++ src/main/java/com/x1/frans/approval/command/application/dto/ApprovalDecisionRequestDTO.java
@@ -1,6 +1,7 @@
 package com.x1.frans.approval.command.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +10,11 @@ import lombok.Setter;
 public class ApprovalDecisionRequestDTO {
 
     @Schema(description = "결재타입")
+    @NotNull(message = "결재 타입은 필수입니다.")
     private String approvalType;
 
     @Schema(description = "결재 상태")
+    @NotNull(message = "결재 상태는 필수입니다.")
     private String status;
 
     @Schema(description = "의견")
