68d57428a39e12109f4ec78be58fcd932201253f
oni128 <lyw283828@gmail.com>
2025-06-16 17:20:03 +0900

    ✨ feat: 결재등록된 결재문서 임시저장상태로 변경(문서회수)

--- src/main/java/com/x1/frans/approval/command/application/controller/ApprovalCommandController.java
+++ src/main/java/com/x1/frans/approval/command/application/controller/ApprovalCommandController.java
@@ -127,4 +127,38 @@ public class ApprovalCommandController {
                 .body(CommonResponse.success(responseDTO, "결재선 템플릿이 삭제되었습니다."));
 
     }
+    @Operation(summary = "결재 수정(재기안 포함)", description = "결재를 수정합니다.")
+    @PutMapping("/{approvalId}")
+    public ResponseEntity<CommonResponse<ApprovalResponseDTO>> modifyApproval(@RequestBody ApprovalCreateRequestDTO request,
+                                                                              @AuthenticationPrincipal CustomUserDetails user,
+                                                                              @PathVariable long approvalId) {
+        long userId = user.getUserId();
+
+        ApprovalResponseDTO responseDTO = approvalCommandService.modifyApproval(request,userId, approvalId);
+
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(CommonResponse.success(responseDTO, "결재 수정이 완료되었습니다."));
+
+    }
+
+    @Operation(summary = " 결재진행 중이던 문서를 임시저장 상태로 변경", description = "결재 중이던 문서를 임시저장상태로 변경합니다.")
+    @PatchMapping("/{approvalId}/draft")
+    public ResponseEntity<CommonResponse<ApprovalResponseDTO>> updateRequestedStatus(@RequestBody ApprovalStatusUpdateDTO request,
+                                                                              @AuthenticationPrincipal CustomUserDetails user,
+                                                                              @PathVariable long approvalId) {
+        long userId = user.getUserId();
+
+        ApprovalResponseDTO responseDTO = approvalCommandService.updateRequestState(request,userId, approvalId);
+
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(CommonResponse.success(responseDTO, "임시저장 상태로 변경되었습니다."));
+
+    }
+
+
+
 }
--- src/main/java/com/x1/frans/approval/command/application/service/ApprovalCommandService.java
+++ src/main/java/com/x1/frans/approval/command/application/service/ApprovalCommandService.java
@@ -16,4 +16,8 @@ public interface ApprovalCommandService {
     Optional<ApprovalResponseDTO> approvalLineTemplatesModify(ApprovalLineTemplateCreateRequestDTO request, long userId, Long templateId);
 
     Optional<ApprovalResponseDTO> deleteApprovalLineTemplates(long userId, Long templateId);
+
+    ApprovalResponseDTO modifyApproval(ApprovalCreateRequestDTO request, long userId, long approvalId);
+
+    ApprovalResponseDTO updateRequestState(ApprovalStatusUpdateDTO request, long userId, long approvalId);
 }
