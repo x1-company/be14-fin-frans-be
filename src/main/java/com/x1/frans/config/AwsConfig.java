209588d31f93ff529fd251bfaffce32a757e3bdd
Sumin <emily9949@naver.com>
2025-06-20 16:30:52 +0900

    🚑 fix: 테스트 환경에서 AWS 에 접근하지 않도록 profile 분리 추가

--- src/main/java/com/x1/frans/config/AwsConfig.java
+++ src/main/java/com/x1/frans/config/AwsConfig.java
@@ -1,6 +1,5 @@
 package com.x1.frans.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -8,7 +7,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 public class AwsConfig {
 
