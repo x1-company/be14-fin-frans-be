7860064f2e4092dee9807127f2143026eb32e130
Sumin <emily9949@naver.com>
2025-06-20 16:39:57 +0900

    🚑 fix: 워크플로우 설정 들여쓰기 오류 수정

--- src/main/java/com/x1/frans/config/AwsConfig.java
+++ src/main/java/com/x1/frans/config/AwsConfig.java
@@ -7,9 +7,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("!test")
 @Configuration
 public class AwsConfig {
 
