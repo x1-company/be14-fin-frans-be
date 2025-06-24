064ec612cb79bef3d65b8db48349ad8cb468bc1e
cxzaqq <roy1228@naver.com>
2025-06-24 17:11:59 +0900

    ✨ feat: 주문 템플릿 등록 수정 삭제 기능 구현

---
+++ src/main/java/com/x1/frans/order/command/application/dto/OrderTemplateDetailDTO.java
@@ -0,0 +1,11 @@
+package com.x1.frans.order.command.application.dto;
+
+import lombok.Getter;
+
+@Getter
+public class OrderTemplateDetailDTO {
+
+    private int quantity;
+    private int seq;
+    private Long productId;
+}
